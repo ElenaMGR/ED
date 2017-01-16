498c7cd4de2649fb5aa7bd311622ebe237a617e1
ElenaMGR <elenamgr@correo.ugr.es>
2017-01-16 11:49:25 +0100

    Practica árboles

--- Practica Arboles/7 Nmer/src/ejemploKtree.cpp
+++ Practica Arboles/7 Nmer/src/ejemploKtree.cpp
@@ -12,7 +12,7 @@
 using namespace std;
 using namespace std::chrono;
 
- 
+
 std::ostream&   operator<<(ostream & s, const pair<char,int> & par){
   s << par.first << " " << par.second;
   return s;
@@ -58,7 +58,7 @@ cout << endl;
 template<typename T, int K>
 int Altura(const ktree<T,K> & arbol){
  return Altura<T,K>(arbol.root());
-  
+
 }
 
 template<typename T, int K>
@@ -80,8 +80,8 @@ if (!n.null()){
 template<typename T, int K>
 void recorrido_preorden(const ktree<T,K> & arbol){
  recorrido_preorden<T,K>(arbol.root());
-  
-} 
+
+}
 
 template<typename T, int K>
 void recorrido_preorden(typename ktree<T,K>::const_node   n){
@@ -101,7 +101,7 @@ if (!n.null()){
 template<typename T, int K>
 void recorrido_inorden(const ktree<T,K> & arbol){
  recorrido_inorden<T,K>(arbol.root());
-  
+
 }
 
 template<typename T, int = 2>
@@ -124,7 +124,7 @@ if (!n.null()){
    if (!n.k_child(i).null())
      cout <<"uso kchild" << *(n.k_child(i)) << endl;
   }
-  
+
   typename ktree<T,K>::node::child_iterator ini = n.begin();
   typename ktree<T,K>::node::child_iterator fin = n.end();
   while (ini!=fin){
@@ -132,7 +132,7 @@ if (!n.null()){
      cout <<"uso iterador " << (*aux)  << ".." << endl;
      ++ini;
    }
-  
+
   for ( typename ktree<T,K>::node hijo : n)
     cout <<  "uso for en rango  " << *hijo<< endl;
   cout <<"sss"<<endl;
@@ -147,14 +147,14 @@ void insertar_valorABB(ktree<T,2> & arbol, const T &valor){
 
   if (arbol.empty())
     arbol.assing(valor);
-  else { 
+  else {
     typename ktree<T,2>::node n = arbol.root();
     bool insertado = false;
     int hijo;
     while (!insertado){
       if ( valor <*n) hijo =0;
       else  hijo = 1;
-         
+
       if (n.k_child(hijo).null()){
 	  arbol.insert_k_child(n, hijo,valor);
 	  insertado = true;
@@ -162,16 +162,16 @@ void insertar_valorABB(ktree<T,2> & arbol, const T &valor){
       else n=n.k_child(hijo);
     }
   }
-  
+
 }
-  
- 
- 
+
+
+
 
 template<typename T, int K>
 void CambiaEtiquetas(typename ktree<T,K>::node  n, const T & antigua, const T & nueva){
 if (!n.null()){
-  if (*n==antigua) 
+  if (*n==antigua)
      *n=nueva;
   for (int i=0; i<K;i++){
    if (!n.k_child(i).null())
@@ -187,7 +187,7 @@ cout <<"Modificamos etiquetas, lo que puede ser error "<< endl;
 if (!n.null()){
 
   cout << "(c" << (*n) <<") ";
-  if (*n==antigua) 
+  if (*n==antigua)
      *n=nueva; <---- NO compila, es const_node y no se puede cambiar su valor
   for (int i=0; i<K;i++){
    if (!n.k_child(i).null())
@@ -201,7 +201,7 @@ if (!n.null()){
 
 
 void TiempoComparativo(  int tama){
- 
+
 ktree<int,2> abb;
 multiset<int> cjto;
 vector<int> datos;
@@ -220,71 +220,71 @@ duration<double> tiempo_transcurrido;
 tantes =  high_resolution_clock::now();
 for (int i = 0; i< tama; i++){
   insertar_valorABB<int>(abb,datos[i]);
- }  
+ }
 tdespues = high_resolution_clock::now();
 tiempo_transcurrido  = duration_cast<duration<double> >(tdespues - tantes);
 cout << "arbol (size "<<abb.size() << ") t= " << tiempo_transcurrido.count() << endl;
 
 
- 
+
 
 tantes = high_resolution_clock::now();
 for (int i = 0; i< tama; i++){
     cjto.insert(datos[i]);
-  
-} 
+
+}
 tdespues =  high_resolution_clock::now();
 tiempo_transcurrido  = duration_cast<duration<double> >(tdespues - tantes);
 cout << "multiset  size( "<< cjto.size() << ") t= " << tiempo_transcurrido.count() << endl;
 
 
-cout << "Altura: abb " << Altura(abb) << " multiset (cota superior) " << 2.0*log2(tama+1.0)<<endl; 
+cout << "Altura: abb " << Altura(abb) << " multiset (cota superior) " << 2.0*log2(tama+1.0)<<endl;
 }
 
 int main(){
- 
+    
 
-ktree<int,2> abb;
+   ktree<int,2> abb;
 
-for (int i=3; i<10; i+=2)
-  insertar_valorABB<int>(abb,i);
-
-
-for (int i=1; i<15; i+=3)
-  insertar_valorABB<int>(abb,i);
-
- 
-cout << "\n Listado hijos de la raiz" <<endl;
-listar_hijos<int,2>(abb.root());
-
-cout << "\n Listado en Inorden" <<endl;
-recorrido_inorden<int,2>(abb);
+   for (int i=3; i<10; i+=2)
+     insertar_valorABB<int>(abb,i);
 
 
-cout << "\n Listado en Preorden" <<endl;
-recorrido_preorden<int,2>(abb);
+   for (int i=1; i<15; i+=3)
+     insertar_valorABB<int>(abb,i);
 
-cout << "\n Listado por Niveles" << endl;
-recorrido_por_niveles<int,2>(abb);
-int org = 5;
-int nv = 0;
-cout << "Modifico etiquetas "<< org << "->" << nv<< endl;
-cout <<"(Si es arbol binario de busqueda puede violar el invariante)" <<endl;
-CambiaEtiquetas<int,2>(abb.root(), org, nv);
 
-cout << "\n Listado en Inorden" <<endl;
-recorrido_inorden<int,2>(abb);
+   cout << "\n Listado hijos de la raiz" <<endl;
+   listar_hijos<int,2>(abb.root());
 
-TiempoComparativo(1000000);
+   cout << "\n Listado en Inorden" <<endl;
+   recorrido_inorden<int,2>(abb);
 
-ktree<pair<char,int>,4> X;
-recorrido_preorden(X);
 
-Nmer prueba;
-prueba.loadSerialized("../datos/cadenaSimple.srl");
-prueba.list_Nmer();
-cout << prueba.size() << " " << prueba.length() << endl;
+   cout << "\n Listado en Preorden" <<endl;
+   recorrido_preorden<int,2>(abb);
 
-return 0;
+   cout << "\n Listado por Niveles" << endl;
+   recorrido_por_niveles<int,2>(abb);
+   int org = 5;
+   int nv = 0;
+   cout << "Modifico etiquetas "<< org << "->" << nv<< endl;
+   cout <<"(Si es arbol binario de busqueda puede violar el invariante)" <<endl;
+   CambiaEtiquetas<int,2>(abb.root(), org, nv);
+
+   cout << "\n Listado en Inorden" <<endl;
+   recorrido_inorden<int,2>(abb);
+
+   TiempoComparativo(1000000);
+
+   ktree<pair<char,int>,4> X;
+   recorrido_preorden(X);
+
+   Nmer prueba;
+   prueba.loadSerialized("../datos/cadenaSimple.srl");
+   prueba.list_Nmer();
+   cout << prueba.size() << " " << prueba.length() << endl;
+
+   return 0;
 
 }
